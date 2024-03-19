fd9404a3b6ecf5c07a091734d63a9cf4a9c13592
Brandon REDACTED <ndn.bebop1337@gmail.com>
2024-03-18 21:22:14 -0500

    First local commit

---
+++ media_collection_manager.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Media Collection Manager
+Version 0.1
+By Brandon REDACTED
+
+A collection of tools built around a central database designed to help build, organize, and maintain a large multimedia collection.
+"""
+
+import os
+import sys
+import getopt
+
+def main(argv):
+	""" Process command line arguments, control program flow
+
+	Arguments:
+		argv - list - Command line arguments
+
+	"""
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
